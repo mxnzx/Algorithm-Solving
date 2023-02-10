5075085bb43a1b3167fb9bec989a7880c3cd72ce
mxnzx <i0692631@naver.com>
2023-02-10 17:51:43 +0900

    4779. 칸토어집합 풀이중

---
+++ BOJ/solution/BOJ4779.java
@@ -0,0 +1,18 @@
+/*
+ * [BOJ]4779. 칸토어 집합
+ * 
+ */
+package solution;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ4779 {
+
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+
+	}
+
+}
