81b67fa3dae3d460e12717740a41718bea6d13f1
mxnzx <i0692631@naver.com>
2023-02-21 00:29:46 +0900

    [BOJ]1074. Z 풀이 완료 - 분할정복

--- BOJ/solution/BOJ1074_Z.java
+++ BOJ/solution/BOJ1074_Z.java
@@ -1,27 +1,27 @@
-/*
+/* [BOJ]1074. Z
  * 대놓고 분할정복 한 곳 탐색했으면 다음 탐색구간으로 가라. 근데 이 과정이 같다 -> 재귀
- * cut()시간초과뜸.
- * 큐?
+ * 분할정복할때 메모리, 시간 생각하고, 무턱대고 재귀를 돌리는게 아니라 조건을 걸어 그 부분만 재귀를 돌릴수있으면 그렇게 하기
+ * 그리고 범위 설정에 유의하자..
  */
 package solution;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class BOJ1074_Z {
 
-	static int N, targetR, targetC, num;
-	static int[][] map;
+	static int N, targetR, targetC, num=0;
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		targetR = sc.nextInt();
-		targetC = sc.nextInt();
-
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		targetR = Integer.parseInt(st.nextToken());
+		targetC = Integer.parseInt(st.nextToken());
 		// 다 들어가고 나올때부터 값을 올리다가 구하고자 하는 값이 나오면 출력하고 리턴.
 		cut(0, 0, (int) Math.pow(2, N));
-
-		// 2차원 배열을 만들
 	}
 	
 	//하나씩 순회하는 방식 -> 시간 초과 뜸
@@ -47,44 +47,33 @@ public class BOJ1074_Z {
 //		}
 //		
 //	}
-
-	//구간을 나누어 내가 찾는 좌표가 어디있는지 조건에 맞춰 찾아간다(좌측상단부터 시계방향으로 1,2,3,4사분면)
+	//구간을 나누어 내가 찾는 좌표가 어디있는지 조건에 맞춰 찾고 거기만 재귀를 돌린다(좌측상단부터 시계방향으로 1,2,3,4사분면)
 	private static void cut(int r, int c, int size) {
-		
-		if(size == 1) {			
-			return;
-		}
-		if(targetR == r && targetC == c) {
 
+		if(r == targetR && c == targetC) {
 			System.out.println(num);
 			return;
 		}
-		
 
 		int half = size/2;
-		int tmp = (int)Math.pow(size/2,2);
+		int tmp = (int)Math.pow(half,2);
+
 		//targetR, targetC가 1사분면
-		
-		if(targetR < half && targetC < half) {
-			System.out.println(1);
-			num += ((int)Math.pow(half,2)*0);
-			cut(r, c, half);
+		if(targetR < r+half && targetC < c+half) {
+			num += 0;
+			cut(r, c, half);		//0 0 4
 			
+		} else if(targetR < r+half && targetC >= c+half) {	//2사분면
+			num += ((int) Math.pow(half, 2));
+			cut(r, c + half, half);
 			
-		} else if(targetR < half && targetC < size) {	//2사분면
-			System.out.println(2);
-			num += ((int)Math.pow(half,2)*1);
-			cut(r, c + tmp, half);
-			
-		} else if(targetR < size && targetC < half) {	//3사분면
-			System.out.println(3);
-			num += ((int)Math.pow(half,2)*2);
-			cut(r + tmp, c, half);
+		} else if(targetR >= r + half && targetC < c+half) {	//3사분면
+			num += ((int)Math.pow(half,2)*2);    //12+2
+			cut(r + half, c, half);          //3,2 1
 			
 		} else {	//4사분면
-			System.out.println(4);
-			num += ((int)Math.pow(half,2)*3);
-			cut(r + tmp, c + tmp, half);
+			num += (int)Math.pow(half,2)*3;			//12
+			cut(r +  half, c + half, half);		//2 2 2
 		}
 	}
 
