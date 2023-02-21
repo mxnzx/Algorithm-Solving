c435b164252f8757c01d70ec348fa49ee3508ad0
mxnzx <i0692631@naver.com>
2023-02-21 17:51:41 +0900

    [BOJ]9663. N-Queen 풀이 완료 - 백트래킹

--- BOJ/solution/BOJ9663_NQueen.java
+++ BOJ/solution/BOJ9663_NQueen.java
@@ -1,12 +1,19 @@
+/*
+ * [BOJ]9663. N-Queen - 백트래킹, 재귀
+ * 일단 다 ~~~~ 돌아야 되는데 가지를 칠 수가 있다. 아닌 곳은 안가는 조건과 처음 시작점을 어떻게 해야할 지 많은 연습이 요구될 듯하다
+ */
+
 package solution;
 
 import java.util.Scanner;
 
 import com.sun.jndi.url.iiopname.iiopnameURLContextFactory;
 
+import sun.print.PSPrinterJob.PluginPrinter;
+
 public class BOJ9663_NQueen {
 	
-	static int N;
+	static int N, Ans;
 	static int[][] map;
 
 
@@ -18,44 +25,44 @@ public class BOJ9663_NQueen {
 		map = new int[N][N];
 		
 		solve(0);
-
+		System.out.println(Ans);
 	}
-	//Queen:1, blank: 0
+
+	//r: 행을 기준으로 볼거야
 	private static void solve(int r) {
 		
-		//basis part
-		if(r==N) {
-			print(map);
+		if(r == N) {
+			Ans++;
 		}
 		
-		
-		//inductive part
-		for (int c = 0; c < N; c++) {
+		//한칸을 움직일 때마다 놓을거야 
+		for(int c=0; c<N; c++) {
+			//놓을 수 없는 체크 조건을 걸어준다
 			if(check(r,c)) {
-				map[r][c] = 1;
+				map[r][c] = 1;	//퀸을 놓았다
 				solve(r+1);
-				map[r][c] = 0;
-				
+				map[r][c] = 0; //복구하고 다음으로			
 			}
 		}
 		
-		
 	}
-	//r,c 위치에 퀸을 놓는 시점에 검사해야 하는 방향은
-	//상, 좌상, 우상 을 검사해야함. 퀸을 놓는 시점이므로 그 전에것을 확인해야지 
-	//퀸이 있으면 false 없으면 true
- 	private static boolean check(int r, int c) {
 
- 		
-		return false;
-	}
-	private static void print(int[][] map) {
-		for (int i = 0; i < map.length; i++) {
-			for (int j = 0; j < map.length; j++) {
-				System.out.print(map[i][j] + " ");
-			}
-			System.out.println();
+
+	private static boolean check(int r, int c) {
+		
+		//우리는 체크를 놓기 전에 본다
+		//상 체크(같은 행)
+		for (int i = r-1; i >= 0 ; i--) {
+			if(map[i][c] == 1) return false;
+		}	//상좌체크
+		for (int i = r-1, j = c-1; i >= 0 && j>=0 ; i--, j--) {
+			if(map[i][j] == 1) return false;
+		}	//상우체크
+		for (int i = r-1, j = c+1; i >= 0 && j < N ; i--, j++) {
+			if(map[i][j] == 1) return false;
 		}
+
+		return true;
 	}
 	
 
