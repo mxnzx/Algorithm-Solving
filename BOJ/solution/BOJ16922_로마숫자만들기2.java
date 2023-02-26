6d30f832cf5589737178b65b6c1f0eed49f34759
mxnzx <i0692631@naver.com>
2023-02-26 22:25:55 +0900

    [BOJ]16922. 로마숫자만들기 풀이 완료 - 중복조합

--- BOJ/solution/BOJ16922_로마숫자만들기2.java
+++ BOJ/solution/BOJ16922_로마숫자만들기2.java
@@ -1,17 +1,18 @@
 /*
  * [BOJ]16922. 로마숫자만들기
- * 중복조합 + 정답 배열
+ * 중복조합 + set
+ * 배열써서 하든 set 써서 하든 시간 똑같음
  */
 
 package solution;
-
+import java.util.HashSet;
 import java.util.Scanner;
 
-public class  BOJ16922_로마숫자만들기 {
+public class BOJ16922_로마숫자만들기2 {
     static int[] arr = {1,5,10,50};
-    static int N,cnt;
+    static int N;
     static int[] sel;
-    static boolean[] ans = new boolean[1001];
+    static HashSet<Integer> ans = new HashSet<>();
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -20,20 +21,20 @@ public class  BOJ16922_로마숫자만들기 {
         sel = new int[N];
 
         comb(0,0);
-        for(boolean v :ans) {
-            if(v) cnt++;
-        }
-        System.out.println(cnt);
+
+        //셋은 중복이 안되니 그냥 사이즈 뽑는다
+        System.out.println(ans.size());
 
 
     }
     private static void comb(int idx, int k) {
-        if(k == N) {
+        if(k == sel.length) {
+            //더해서 정답 셋에 넣어준다
             int sum=0;
             for(int i : sel) {
                 sum+=i;
             }
-            ans[sum] = true;
+            ans.add(sum);
             return;
         }
 
