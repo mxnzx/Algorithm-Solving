64c0bbf3c7436cf855327da0cb9d81b4e1b66394
mxnzx <i0692631@naver.com>
2023-02-27 23:44:23 +0900

    [SWEA]3289. 서로소집합 풀이 완료 - 자료구조(그래프), 서로소집합

--- SWEA/solution/SWEA3289_서로소집합.java
+++ SWEA/solution/SWEA3289_서로소집합.java
@@ -1,5 +1,6 @@
 /*
- * [SWEA]3289. 서로소집합
+ * [SWEA]3289. 서로소집합 - 자료구조(그래프), 서로소집합
+ * 막힌 부분: 교집합 찾는 부분에서 루트노드를 안구했었음
  */
 
 package solution;
@@ -30,14 +31,42 @@ public class SWEA3289_서로소집합 {
 			for (int i = 1; i <= n; i++) {
 				parents[i] = i;
 			}
-			
+			sb.append("#").append(tc).append(" ");
 			for (int i = 0; i < m; i++) {
+				st = new StringTokenizer(br.readLine());
+				int c = Integer.parseInt(st.nextToken());
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				if(c == 0) {	//합집합 실행
+					//union - find
+					union(a,b);
+
+				} else { //c=1 일때. 둘의 교집합 여부 확인 -> 루트노드를 찾아 교집합인지 확인해라
+					int pa = find(a);
+					int pb = find(b);
+					if(pa == pb) {
+						sb.append(1);
+					} else sb.append(0);
+				}
 				
 			}
+			sb.append("\n");
 
 		}
 		System.out.println(sb);
 
+	}
+	private static void union(int a, int b) {
+		int pa = find(a);
+		int pb = find(b);
+
+		if(pa != pb) {
+			parents[pb] = parents[pa];
+		}
+	}
+	private static int find(int og) {
+		if(parents[og] == og) return og;
+		else return parents[og] = find(parents[og]);
 	}
 
 }
