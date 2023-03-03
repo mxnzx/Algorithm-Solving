96a85b73ae8caed200a04d74751438ae41c55482
mxnzx <i0692631@naver.com>
2023-03-03 17:30:24 +0900

    풀이중

---
+++ SWEA/solution/SWEA1767_프로세서연결하기.java
@@ -0,0 +1,15 @@
+package solution;
+
+/*
+ * 많은 전원을 연결하는 게 우선 - 전부다 찾아가면서 연결한다
+ */
+public class SWEA1767_프로세서연결하기 {
+	static int N;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
