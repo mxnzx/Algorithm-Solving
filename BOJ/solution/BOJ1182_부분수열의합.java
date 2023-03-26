89359e61630b3c3655438b562f100c3b301f2486
mxnzx <i0692631@naver.com>
2023-03-26 13:33:56 +0900

    [BOJ]1182. 부분수열의합 - 조합론(부분집합), 백트래킹

--- BOJ/solution/BOJ1182_부분수열의합.java
+++ BOJ/solution/BOJ1182_부분수열의합.java
@@ -1,10 +1,62 @@
 package solution;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class BOJ1182_부분수열의합 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	static int N,S,res=0;
+	static int[] arr;
+	static boolean[] sel;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		S = Integer.parseInt(st.nextToken());
 
+		arr = new int[N];
+		sel = new boolean[N];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		//부분집합문제
+
+		func(0,0);
+		System.out.println(res);
+	}
+	//부분집합 고르기
+	private static void func(int idx, int k) {
+
+
+		if(idx == arr.length) {
+			checkS();
+			return;
+		}
+
+		sel[idx] = true;
+		func(idx+1,k+1);
+
+		sel[idx]= false;
+		func(idx+1,k);
+
+	}
+	//고른 원소값이 S가 되는지
+	private static void checkS() {
+		int sum=0;
+		boolean flag = false;	//공집합 체크 플래그
+		for (int i = 0; i < N; i++) {
+			if(sel[i]) {
+				sum+=arr[i];
+				flag = true;
+			}
+		}
+		if(!flag) return;
+		if(sum == S) res++;
 	}
 
 }
