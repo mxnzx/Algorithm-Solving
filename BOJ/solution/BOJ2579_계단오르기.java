e74ad6c48f159360723ff019e8930c78ed3cf89f
mxnzx <i0692631@naver.com>
2023-03-29 17:57:19 +0900

    풀이중

---
+++ BOJ/solution/BOJ2579_계단오르기.java
@@ -0,0 +1,23 @@
+package solution;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ2579_계단오르기 {
+
+	static int n;
+	static int[] score, maxScore;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
+		score = new int[n];
+		for (int i = 0; i < n; i++) {
+			score[i] = Integer.parseInt(br.readLine());
+		}
+		maxScore = new int[n];
+		
+		
+	}
+
+}
