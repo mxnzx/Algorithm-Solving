1551d64a109881f4577818cdd6824cd5f7a6b11f
mxnzx <i0692631@naver.com>
2023-03-31 07:20:04 +0900

    풀이중

--- BOJ/solution/BOJ14502_연구소.java
+++ BOJ/solution/BOJ14502_연구소.java
@@ -3,16 +3,24 @@ package solution;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class BOJ14502_연구소 {
+	static class Point {
+		int r,c;
 
+		public Point(int r, int c) {
+			this.r = r;
+			this.c = c;
+		}
+	}
 	static int N,M;
 	static int[][] map;
 	static boolean[][] v;
 	static int[] dr= {-1,1,0,0};
 	static int[] dc= {0,0,-1,1};
-	
+	static ArrayList<Point> zeroList = new ArrayList<>();
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
@@ -26,17 +34,30 @@ public class BOJ14502_연구소 {
 			st =new StringTokenizer(br.readLine());
 			for (int j = 0; j < M; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
+				if(map[i][j] == 0) {
+					zeroList.add(new Point(i,j));
+				}
 			}
 		}
-		backtracking(0,0);
+		//0인 애들을 찾아서 리스트에 저장. 걔를 빼면서 놓을래 안놓을래 -> 재귀 cnt=3 되면 checkMax
+		backtracking(0, 0);
 	}
-
-	private static void backtracking(int r, int c) {
+	//3개 고른다
+	private static void backtracking(int idx, int cnt) {
 		
-		if(r == N-1 && c == M-1) {
+		if(cnt == 3) {
 			checkMax();
 			return;
 		}
+		//현재 0
+		Point p = new Point(zeroList.get(idx).r, zeroList.get(idx).r);
+		//고른경우
+		map[p.r][p.c] = 1;
+		backtracking(idx+1, cnt+1);
+		map[p.r][p.c] = 0;
+
+		//안고른경
+		backtracking(idx+1, cnt);
 		
 	}
 
