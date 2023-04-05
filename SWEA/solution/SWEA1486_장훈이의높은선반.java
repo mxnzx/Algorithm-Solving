f986ad7970fc18e18ff9cc7b50d9c4589dd72240
mxnzx <i0692631@naver.com>
2023-04-05 19:57:00 +0900

    [SWEA]1486. 장훈이의높은선반 풀이 완료 - 부분집합

--- SWEA/solution/SWEA1486_장훈이의높은선반.java
+++ SWEA/solution/SWEA1486_장훈이의높은선반.java
@@ -7,7 +7,7 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class SWEA1486_장훈이의높은선반 {
-	static int T, N, B, S;
+	static int T, N, B;
 	static int[] height;
 
 	static StringBuilder sb = new StringBuilder();
@@ -21,47 +21,46 @@ public class SWEA1486_장훈이의높은선반 {
 			N = Integer.parseInt(st.nextToken());
 			B = Integer.parseInt(st.nextToken());	//달성해야 할 높이
 			height = new int[N];
+			min = Integer.MAX_VALUE;
 			st= new StringTokenizer(br.readLine());
 			for (int i = 0; i < N; i++) {
 				height[i] = Integer.parseInt(st.nextToken());
-				S += height[i];
 			}
-			System.out.println(S + " " + B);
 			//만족하는 S의 최솟값을 찾는 문제(필요한 점원의 명수는 고려하지 않는다)
 			//B보다는 커야하지만 그 중 제일 작은 수 - 순조부같은데
 			
 			//부분집합
 			//다~~~~~~~~~구하자
-			for (int i = 0; i < N; i++) {
-				comb(0,0,new int[i], 0);
+
+			//부분집합 - 조합을 반복해서
+			for (int i = 0; i <= N; i++) {
+				comb(0,0, i, 0);
 			}
-			System.out.println(min);
+			sb.append("#").append(tc).append(" ").append(min).append("\n");
 			
 		}
+		System.out.println(sb);
 		
 		
 
 	}
-	static int min=Integer.MAX_VALUE;
-	private static void comb(int idx, int k, int[] sel, int sum) {
+	static int min;
+	//조합 idx: 원본배열 인덱스  k:뽑은 개수  choiceNum: 선택해야하는 개수  sum: 합계
+	private static void comb(int idx, int k, int choiceNum, int sum) {
 		
-		if(k ==sel.length) {
-			//System.out.println(sum);
-			if(sum>=S) {
-				min = Math.min(sum-S, min);
-				System.out.println(min);
-				
-			}
+		if(k == choiceNum) {
+			//뽑으려는 개수만큼 뽑혔다면 B값을 넘겼는지 체크하고 최솟값 비교하고 갱신
+			if(sum>=B) min = Math.min(sum-B, min);
 			return;
 		}
-		if(idx == height.length) {
-			return;
-		}
+		//원본배열 인덱스가 배열 끝까지 돌았다면 리턴
+		if(idx == height.length) return;
+		if(min == 0) return;
 		
 		//고르는 경우
-		comb(idx+1, k+1, sel, sum+height[idx]);
+		comb(idx+1, k+1, choiceNum, sum+height[idx]);
 		//안고르는 경우
-		comb(idx+1, k, sel, sum);
+		comb(idx+1, k, choiceNum, sum);
 	}
 
 }
