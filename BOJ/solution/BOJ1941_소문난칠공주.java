d3fe68bc3e53b1c01569d6449024888c22169990
mxnzx <i0692631@naver.com>
2023-04-13 17:50:12 +0900

    풀이중 - 이걸ㄹ로 다시 풀기

--- BOJ/solution/BOJ1941_소문난칠공주.java
+++ BOJ/solution/BOJ1941_소문난칠공주.java
@@ -5,6 +5,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -12,12 +13,21 @@ public class BOJ1941_소문난칠공주 {
 
 	static class Pos {
 		int r,c;
+		char val;
 
-		public Pos(int r, int c) {
+		public Pos(int r, int c, char val) {
 			super();
 			this.r = r;
 			this.c = c;
+			this.val = val;
 		}
+
+		@Override
+		public String toString() {
+			return "Pos [r=" + r + ", c=" + c + ", val=" + val + "]";
+		}
+		
+		
 		
 	}
 	static int res;
@@ -25,74 +35,90 @@ public class BOJ1941_소문난칠공주 {
 	static boolean[][] v;
 	static int[] dr = {-1,1,0,0};
 	static int[] dc = {0,0,-1,1};
-	static ArrayList<Pos> som, yeon;
+	static ArrayList<Pos> alist;
 	public static void main(String[] args) throws IOException {
 		System.setIn(new FileInputStream("input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		map = new char[5][5];
 		v= new boolean[5][5];
-		som = new ArrayList<>();
-		yeon = new ArrayList<>();
+		alist = new ArrayList<>();
 		for (int i = 0; i < 5; i++) {
 			String str = br.readLine();
 			for (int j = 0; j < 5; j++) {
 				map[i][j] = str.charAt(j);
-				if(map[i][j] == 'S') som.add(new Pos(i,j));
-				if(map[i][j] == 'Y') yeon.add(new Pos(i,j));
+				alist.add(new Pos(i,j,map[i][j]));
 			}
 		}
 		//dfs가 안됨 - 다시 되돌아가는 구조이므로
-		//25개라고 한정되어있으니까 25개 중에 7개를 뽑는다 -> 일단 S를 먼저 뽑고 -> 나머지 Y를 고른다 (4,3) (5,2) (6,1) (7,0)
-		//S가 몇 개 있는지 체크하고 4~N개 까지 파워셋?
+		//25개라고 한정되어있으니까 25개 중에 7개를 뽑는다
 		//하고 나서 bfs나 서로소집합을 이용해 하나로 이어져있는지 확인한다
-		//선택하면 그걸 O로 바꾸자
-		int per = som.size()-4;	//5-4=1
-		for (int i = 0; i <= (per = som.size()>7?3:per); i++) {
-			//다솜이를 4+i개 뽑는다
-			somComb(0,0,new Pos[4+i]);
-		}
-		
+		//그리고 S가 4개 이상인 지 확인
+		comb(0,0,new Pos[7]);
 		System.out.println(res);
-		
-
 	}
 
-	private static void somComb(int idx, int k, Pos[] sel) {
-		if(k==sel.length) {
-			//임도연배열과 이다솜배열을 합쳐서 뭉쳐있는지 확인한다
-			Pos[] addComb = new Pos[7];
-			System.arraycopy(sel,0,addComb,0,sel.length);
-			Pos[] p = yeonComb(0,0,new Pos[7-sel.length]);
-			System.arraycopy(p,0,addComb,sel.length, p.length);
-			//p가 다 붙어있는지 확인한다
-			checkArray(p);
+	
+
+	private static void comb(int idx, int k, Pos[] sel) {
+		
+		if(k == sel.length) {
+			//뽑은 리스트의 인덱스배열이 나온다
+			//System.out.println(Arrays.toString(sel));
+			if(checkArray(sel)) {
+				res++;
+			}
 			return;
 		}
-		for (int i = idx; i < som.size(); i++) {
-			sel[k] = som.get(i);
-			somComb(i+1, k+1, sel);
+		for (int i = idx; i < 25; i++) {
+			sel[k] = alist.get(i);
+			comb(i+1, k+1, sel);
 		}
+		
 	}
 
-	private static void checkArray(Pos[] p) {
+	private static boolean checkArray(Pos[] choiceArr) {
+		v = new boolean[5][5];
 		//bfs사용해서 확인한다. 맞으면 res++
-
+		int cnt=0;
 		Queue<Pos> q = new LinkedList<>();
-		]
-		q.add(new Pos());
-
+		q.add(choiceArr[0]);
+		v[choiceArr[0].r][choiceArr[0].c] = true;
+		
+		while(!q.isEmpty()) {
+			Pos p = q.poll();
+			
+			for (int d = 0; d < 4; d++) {
+				int nr = p.r + dr[d];
+				int nc = p.c + dc[d];
+				
+				for(Pos pp : choiceArr) {
+					if(nr == pp.r && nc == pp.c && !v[nr][nc]) {
+						System.out.println(nr + " " + nc);
+						v[nr][nc] = true;
+						q.add(new Pos(nr, nc, map[nr][nc]));
+						cnt++;
+					}
+				}
+			}
+		}
+		
+		if(cnt==7) {		
+			if(checkSom(choiceArr)) {
+				return true;
+			}
+			return false;
+		}
+		return false;
 	}
 
-	private static Pos[] yeonComb(int idx, int k, Pos[] sel) {
-		if(k==sel.length) {
-			return sel;
+	private static boolean checkSom(Pos[] p) {
+		int cnt=0;
+		for(Pos pp : p) {
+			if(pp.val == 'S') cnt++;
 		}
-		for (int i = idx; i < yeon.size(); i++) {
-			sel[k] = yeon.get(i);
-			yeonComb(i+1, k+1, sel);
-		}
-		return sel;
+		//cnt가 4개 이상이면 true
+		if(cnt>=4) return true;
+		return false;
 	}
-
 
 }
