141026b67e856fa6da844fd4f95cf62e7adb474f
mxnzx <i0692631@naver.com>
2023-04-21 17:50:09 +0900

    [BOJ]16398. 행성연결 - MST

--- BOJ/solution/BOJ16398_행성연결.java
+++ BOJ/solution/BOJ16398_행성연결.java
@@ -4,82 +4,92 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Iterator;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class BOJ16398_행성연결 {
-    static class Node {
-        int e,c;
+	static class Node {
+		int e, c;
 
-        public Node(int e, int c) {
-            this.e = e;
-            this.c = c;
-        }
+		public Node(int e, int c) {
+			this.e = e;
+			this.c = c;
+		}
 
-        public int getC() {
-            return c;
-        }
+		public int getC() {
+			return c;
+		}
 
-    }
-    static int[][] adjMatrix;
-    static int[] dist;
-    static boolean[] v;
-    static int N;
-    public static void main(String[] args) throws IOException {
-        System.setIn(new FileInputStream("input.txt"));
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+	}
 
-        N = Integer.parseInt(br.readLine());
-        adjMatrix = new int[N][N];
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
-                adjMatrix[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
+	static int[][] matrix;
+	static ArrayList<Node>[] adjList;
+	static int[] dist;
+	static boolean[] v;
+	static int N;
 
-        v = new boolean[N];
-        dist = new int[N];
-        Arrays.fill(dist, Integer.MAX_VALUE);
+	public static void main(String[] args) throws IOException {
+		System.setIn(new FileInputStream("input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 
-        //인덱스 0번의 노드부터 탐색하며
-        dist[0]=0;
-        prim(0);
+		N = Integer.parseInt(br.readLine());
 
+		// 인접리스트로 구현
+		adjList = new ArrayList[N];
+		for (int i = 0; i < N; i++) {
+			adjList[i] = new ArrayList<>();
+		}
+		int tmp;
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				tmp = Integer.parseInt(st.nextToken());
+				if (i >= j)
+					continue;
+				adjList[i].add(new Node(j, tmp));
+				adjList[j].add(new Node(i, tmp));
+			}
 
-    }
+		}
 
-    private static void prim(int start) {
-        PriorityQueue<Node> q = new PriorityQueue<>(Comparator.comparing(Node::getC));
-        q.add(new Node(0,0));
+		v = new boolean[N];
+		dist = new int[N];
+		Arrays.fill(dist, Integer.MAX_VALUE);
 
-        long sum = 0;
+		// 인덱스 0번의 노드부터 탐색하며 갱신한다
+		dist[0] = 0;
+		long sum = prim(new Node(0, 0));
 
-        while (!q.isEmpty()) {
-            Node p = q.poll();
+		System.out.println(sum);
 
+	}
 
-            int next;
-            if(!v[p.e]) {
-                v[p.e] = true;
-                sum += p.e;
-                for (int i = 0; i < N; i++) {
+	private static long prim(Node start) {
+		PriorityQueue<Node> q = new PriorityQueue<>(Comparator.comparing(Node::getC));
+		q.add(start);
 
-                        if(adjMatrix[i][j] != 0) {
-                            next = adjMatrix[i][j];
-                            if (!v[next] && dist[p.e] > next) {
-                                dist[p] = next;
-                                q.add(new Node(i, ))
-                            }
-                        }
+		long sum = 0;
 
-                }
-            }
+		while (!q.isEmpty()) {
+			Node p = q.poll();
 
+			if (v[p.e]) continue;
+			
+			v[p.e] = true;
+			sum += p.c;
+			for (Node next : adjList[p.e]) {
+				if (!v[next.e] && dist[next.e] > next.c) {
+					dist[p.e] = next.c;
+					q.add(next);
+				}
+			}
+		}
 
-        }
-    }
+		return sum;
+	}
 }
