0e4e73976b56f5d862deb15a5c70f6921c668bc2
mxnzx <i0692631@naver.com>
2023-05-10 09:37:57 +0900

    [BOJ]1938. 통나무옮기기 - bfs

--- BOJ/solution/BOJ1938_통나무옮기기.java
+++ BOJ/solution/BOJ1938_통나무옮기기.java
@@ -11,10 +11,9 @@ import java.util.Queue;
 public class BOJ1938_통나무옮기기 {
 
     static class Node {
-        int r, c, cnt;
-        boolean type;   //true:가로   false:세로
-
-        public Node(int r, int c, boolean type, int cnt) {
+        int r, c, type, cnt;
+        
+        public Node(int r, int c, int type, int cnt) {
             this.r = r;
             this.c = c;
             this.type = type;
@@ -63,17 +62,14 @@ public class BOJ1938_통나무옮기기 {
         //들어온 stick과 end가 가로인지 세로인지 확인
         checkType(stick);
         checkType(end);
-        //System.out.println(stick[0].type);
-        //System.out.println(end[0].type);
 
-        print(map);
         bfs();  //제일 처음 [0]만 가지고 bfs 실행한다
         System.out.println(res);
 
     }
 
     private static void checkType(Node[] arr) {
-        boolean type = arr[0].r == arr[1].r;    //true:가로   false:세로
+        int type = arr[0].r == arr[1].r?0:1;    //0:가로   1:세로
         for (int i = 0; i < arr.length; i++) {
             arr[i].type = type;
         }
@@ -82,6 +78,7 @@ public class BOJ1938_통나무옮기기 {
     private static void bfs() {
         Queue<Node> q = new LinkedList<>();
         q.add(stick[0]);
+        v[stick[0].r][stick[0].c][stick[0].type] = true;
 
         while (!q.isEmpty()) {
             Node p = q.poll();
@@ -96,37 +93,39 @@ public class BOJ1938_통나무옮기기 {
                 int nr = p.r + dr[d];
                 int nc = p.c + dc[d];
 
-                if(p.type) {   //현재 방향이 가로
+                if(p.type == 0) {   //현재 방향이 가로
                     //가장자리 체크 - 행(r)은 하나만 봐도되고, 열(c)는 맨오른쪽도 봐야함 && 방문배열(가로)도 체크
                     if(nr<0 || nr>=N || nc<0 || (nc+2)>=N || v[nr][nc][0]) continue;
                     //가려는 곳 3칸이 전부 1이 없는 지 확인
                     if(map[nr][nc] == 1 || map[nr][nc+1] == 1 || map[nr][nc+2] == 1) continue;
-                    q.add(new Node(nr,nc, true,p.cnt+1));
-
+                    q.add(new Node(nr,nc, 0,p.cnt+1));
                     v[nr][nc][0] = true;
                 } else {    //현재 방향이 세로
                     //가장자리 체크 - 열(c)은 하나만 봐도 되고, 행(r)은 맨오른쪽도 봐야함 && 방문배열 체크
                     if(nr<0 || (nr+2)>=N || nc<0 || nc>=N || v[nr][nc][1]) continue;
                     //가려는 곳 3칸 전부 1이 없는 지 확인
                     if(map[nr][nc] == 1 || map[nr+1][nc] == 1 || map[nr+2][nc] == 1) continue;
-                    q.add(new Node(nr,nc, false,p.cnt+1));
+                    q.add(new Node(nr,nc, 1,p.cnt+1));
                     v[nr][nc][1] = true;
                 }
             }
             //회전
             if (check33(p.r, p.c, p.type)) {
                 //회전한다
-                q.add(new Node(p.r,p.c, !p.type,p.cnt+1));
-                if(p.type) v[p.r][p.c][1] = true;    //가로면 세로
-                else v[p.r][p.c][0] = true;          //세로면 가로
+            	if(p.type == 0 && !v[p.r-1][p.c+1][1]) {
+            		q.add(new Node(p.r-1,p.c+1, 1, p.cnt+1));
+            		v[p.r-1][p.c+1][1] = true;    //가로면 세로
+            	} else if(p.type == 1 && !v[p.r+1][p.c-1][0]) {
+            		q.add(new Node(p.r+1,p.c-1, 0, p.cnt+1));
+            		v[p.r+1][p.c-1][0] = true;          //세로면 가로
+            	}
             }
         }
     }
 
     //3*3 갈수 있고, 다 비어있는지 체크
-    private static boolean check33(int r, int c, boolean type) {
-        System.out.println(r + " " + c + " " +type);
-        if(type) {  //가로면 위에 3줄, 아래 3줄
+    private static boolean check33(int r, int c, int type) {
+        if(type==0) {  //가로면 위에 3줄, 아래 3줄
             if(r-1<0 || r+1>=N) return false;
             for(int i = r-1; i <= r + 1; i++) {
                 for (int j = c; j <= c + 2; j++) {
@@ -143,14 +142,4 @@ public class BOJ1938_통나무옮기기 {
         }
         return true;
     }
-
-    private static void print(int[][] map) {
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                System.out.print(map[i][j] + "\t");
-            }
-            System.out.println();
-        }
-    }
-
 }
