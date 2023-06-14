c6dfd1a554c1c607179cb3b990a343e374d9ca17
mxnzx <i0692631@naver.com>
2023-06-14 23:15:03 +0900

    [BOJ]10025. 게으른백곰 - 슬라이딩윈도우

--- BOJ/solution/BOJ10025_게으른백곰.java
+++ BOJ/solution/BOJ10025_게으른백곰.java
@@ -20,33 +20,23 @@ public class BOJ10025_게으른백곰 {
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
         arr = new int[1000001];
-        int gram, pos, maxPos=0;
-        int totalSum = 0;
+        int gram, pos;
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             gram = Integer.parseInt(st.nextToken());
             pos = Integer.parseInt(st.nextToken());
             arr[pos] = gram;
-            maxPos = Math.max(maxPos, pos);
-            totalSum += gram;
+
         }
         int size = 2*K+1;
-        if(size > arr.length) {     //전체 합 내놓고 바로 종료
-            System.out.println(totalSum);
-            System.exit(0);
-        }
         int sum=0;
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < ((size<=arr.length)? size : arr.length); i++) {
             sum += arr[i];
         }
         int max = sum;
-        int start = 0;
-        int end = size;
-        while(end<=maxPos) {
-            sum = sum - arr[start] + arr[end];
+        for (int i = size; i < arr.length; i++) {
+            sum = sum - arr[i-size] + arr[i];
             max = Math.max(max, sum);
-            start++;
-            end++;
         }
         System.out.println(max);
     }
