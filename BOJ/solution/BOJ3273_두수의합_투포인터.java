21afa0133930e7b9ce0a84f5b688f154af192ec7
mxnzx <i0692631@naver.com>
2023-06-15 00:22:35 +0900

    [BOJ]3273. 두 수의 합 : 투포인터

--- BOJ/solution/BOJ3273_두수의합_투포인터.java
+++ BOJ/solution/BOJ3273_두수의합_투포인터.java
@@ -1,39 +1,36 @@
-//계수 정렬
-
 package solution;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class BOJ3273 {
+public class BOJ3273_두수의합_투포인터 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         int n = Integer.parseInt(br.readLine());
-
-        int[] arr = new int[2000002];
-
+        int[] arr = new int[n];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            arr[Integer.parseInt(st.nextToken())]++;
+            arr[i] = Integer.parseInt(st.nextToken());
         }
         int x = Integer.parseInt(br.readLine());
-
-        int count = 0;
-        for (int i = 0; i < x; i++) {
-            if(arr[i] == 1) {
-                int searchNum = x - i;
-                //searchNum 값이 그 뒤에 있다면 count++
-                if(arr[searchNum] == 1)
-                    count++;
+        Arrays.sort(arr);
+        int count = 0, start=0, end=arr.length-1, sum;
+        while(start<end) {
+            sum = arr[start] + arr[end];
+            if(sum == x) {
+                start++;
+                end--;
+                count++;
             }
+            else if (sum > x) end--;
+            else start++;
+
         }
-
-        //중복된 count 빼줌
-        System.out.println(count/2);
+        System.out.println(count);
     }
-
 }
