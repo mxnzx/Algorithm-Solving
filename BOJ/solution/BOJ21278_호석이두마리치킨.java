00f48f79dd89e84297cb920051429af8989f208a
mxnzx <i0692631@naver.com>
2023-07-08 00:04:18 +0900

    [BOJ]21278. 호석이두마리치킨 : 조합,bfs(플로이드워샬쓰면 훨씬 빠름)

--- BOJ/solution/BOJ21278_호석이두마리치킨.java
+++ BOJ/solution/BOJ21278_호석이두마리치킨.java
@@ -4,24 +4,22 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class BOJ21278_호석이두마리치킨 {
 
-    static int N,M,result=Integer.MAX_VALUE;
+    static int N, M, result = Integer.MAX_VALUE;
     static ArrayList<Integer>[] adjList;
     static int[] resultNode = new int[2];
+
     static class Node {
         int node, cnt;
+
         public Node(int node, int cnt) {
             this.node = node;
             this.cnt = cnt;
         }
     }
-
     public static void main(String[] args) throws IOException {
         System.setIn(new FileInputStream("input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -29,7 +27,7 @@ public class BOJ21278_호석이두마리치킨 {
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        adjList = new ArrayList[N+1];
+        adjList = new ArrayList[N + 1];
         for (int i = 1; i <= N; i++) {
             adjList[i] = new ArrayList<>();
         }
@@ -42,47 +40,43 @@ public class BOJ21278_호석이두마리치킨 {
             adjList[to].add(from);
         }
         //2개 뽑고 -> 완탐
-        comb(1,0,new int[2]);
-        System.out.println(resultNode[0] + " " + resultNode[1] + " " + result*2);
+        comb(1, 0, new int[2]);
+        System.out.println(resultNode[0] + " " + resultNode[1] + " " + result * 2);
 
     }
 
     private static void comb(int idx, int k, int[] sel) {
-        if(k == sel.length) {
+        if (k == sel.length) {
             int tmp = calc(sel);
-            if(result > tmp) {
+            if (result > tmp) {
                 result = tmp;
                 resultNode[0] = sel[0];
                 resultNode[1] = sel[1];
             }
             return;
         }
-        if(idx == adjList.length) return;
+        if (idx == adjList.length) return;
         for (int i = idx; i <= N; i++) {
             sel[k] = i;
-            comb(i+1, k+1, sel);
+            comb(i + 1, k + 1, sel);
         }
     }
 
     private static int calc(int[] sel) {
         int sum = 0;
-        for (int i = 1; i <= N; i++) {
-            boolean[] v = new boolean[N+1];
-            if(i == sel[0] || i == sel[1]) continue;
-            Queue<Node> q = new LinkedList<>();
-            q.add(new Node(i,0));
-            v[i] = true;
-            while(!q.isEmpty()) {
-                Node present = q.poll();
-                if(present.node == sel[0] || present.node == sel[1]) {
-                    sum+=present.cnt;
-                    break;
-                }
-                for(int next : adjList[present.node]) {
-                    if(!v[next]) {
-                        q.add(new Node(next, present.cnt+1));
-                        v[next]=true;
-                    }
+        boolean[] v = new boolean[N + 1];
+        Queue<Node> q = new LinkedList<>();
+        q.add(new Node(sel[0], 0));
+        q.add(new Node(sel[1], 0));
+        v[sel[0]] = true;
+        v[sel[1]] = true;
+        while (!q.isEmpty()) {
+            Node present = q.poll();
+            for (int next : adjList[present.node]) {
+                if (!v[next]) {
+                    q.add(new Node(next, present.cnt + 1));
+                    v[next] = true;
+                    sum += present.cnt+1;
                 }
             }
         }
