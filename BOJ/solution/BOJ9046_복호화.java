b34fefde2f3039f9123239f6d1c97cd6728c3c31
mxnzx <i0692631@naver.com>
2023-07-12 05:03:15 +0900

    [BOJ]9046.복호화 : 문자열

--- BOJ/solution/BOJ9046_복호화.java
+++ BOJ/solution/BOJ9046_복호화.java
@@ -5,34 +5,32 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class BOJ9046_복호화 {
-    static int[] alpabet = new int[26];
+    static int[] alpabet;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine());
         for (int tc = 0; tc < T; tc++) {
+            alpabet = new int[26];
             String str = br.readLine();
-            str.replace(" ","");
+            str = str.replace(" ","");
+
             int maxValue = -1;
             for (int i = 0; i < str.length(); i++) {
-                System.out.println(str.charAt(i)-'a');
                 alpabet[str.charAt(i)-'a']++;
                 maxValue = Math.max(alpabet[str.charAt(i)-'a'], maxValue);
             }
             int maxValueCnt=0;
             int maxValueIndex=0;
-            boolean flag = false;
+
             for (int i = 0; i < 26; i++) {
-                if(maxValueCnt>1)  {
-                    flag = true;
-                    break;
-                }
                 if(alpabet[i] == maxValue) {
                     maxValueIndex = i;
                     maxValueCnt++;
                 }
             }
-            System.out.println(flag ? "?" : (char) (maxValueIndex + '0'));
+            sb.append(maxValueCnt == 1 ? (char) (maxValueIndex + 'a') : "?").append("\n");
         }
+        System.out.println(sb);
     }
 }
