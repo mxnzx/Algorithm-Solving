9368a510e10fe166bdbe9048c9fd16a81382aa0e
Minji Ryu <77240765+mxnzx@users.noreply.github.com>
2023-10-13 01:28:14 +0900

    [B OJ]1449.수리공항승 - 그리디

--- BOJ/solution/BOJ1449_수리공항승.java
+++ BOJ/solution/BOJ1449_수리공항승.java
@@ -19,29 +19,16 @@ public class BOJ1449_수리공항승 {
         for (int i = 0; i < N; i++) {
             pos[Integer.parseInt(st.nextToken())]++;
         }
-        /*
-        0 1100000....11.....
-        돌다가 1이나오면 0 1  1 1 1 1 0 이 나오면
-         */
-        int n=1;
-        int needCnt = 0;
-        while(n < 1001) {
-            if(pos[n] < 1) continue;
-            int end = counting(n,n+1);
-            //n이 시작이고, end가 끝이므로 거리 계산
-            int length = end - n;
-            if(length < N) {
-                needCnt +=1;
-                n = end;
+        int n = 1, answer = 0;
+        while(n <= 1000) {
+            if(pos[n] == 0) {
+                n++;
                 continue;
             }
-            needCnt = length / 2; //3/2=1
-
+            n += L;     //
+            answer++;
         }
+        System.out.println(answer);
 
-    }
-    private static int counting(int start, int n) {
-        if(pos[n] == 1 && n <= 1000) counting(start, n+1);
-        else return n-1;
     }
 }
