a04fb4f2813d969643594196d6a7bfb10e957a6a
Minji Ryu <77240765+mxnzx@users.noreply.github.com>
2024-02-08 00:04:21 +0900

    [BOJ]4659.비밀번호발음하기 - 구현, 문자열

--- BOJ/solution/BOJ4659_비밀번호발음하기.java
+++ BOJ/solution/BOJ4659_비밀번호발음하기.java
@@ -5,41 +5,61 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class BOJ4659_비밀번호발음하기 {
+    static final char[] vowel = {'a', 'e', 'i', 'o', 'u'};
+    static StringBuilder sb;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+        sb = new StringBuilder();
         String input;
-        while(!(input = br.readLine()).equals("end")) {
+        while (!(input = br.readLine()).equals("end")) {
             char[] inputArr = input.toCharArray();
-            requirement2(inputArr);
-            //aa,ii,uu가 나오지 않으면 통과
-            System.out.println(input);
-            //세개가 연속되지 않고,
-            for (int i = 0; i < ; i++) {
-
+            boolean isIncludeVowel = false;
+            int consCnt = 0;    // 자음 개수
+            int vowelCnt = 0;   // 모음 개수
+            boolean isAcceptable = true;
+            char prev = ' ';
+            for (int i = 0; i < inputArr.length; i++) {
+                char c = inputArr[i];
+                boolean isVowel = false;
+                // 요구사항 1
+                for (char v : vowel) {
+                    if (c == v) {
+                        isIncludeVowel = true;
+                        isVowel = true;
+                        break;
+                    }
+                }
+                // 요구사항 2
+                if(isVowel) {
+                    vowelCnt++;
+                    consCnt = 0;
+                } else {
+                    consCnt++;
+                    vowelCnt = 0;
+                }
+                if(vowelCnt == 3 || consCnt == 3) {
+                    isAcceptable = false;
+                    break;
+                }
+                // 요구사항 3
+                if(c == 'e' || c == 'o') continue;
+                if(c == prev) {
+                    isAcceptable = false;
+                    break;
+                }
+                prev = c;
             }
-            if(!requirement3(input)) continue;
-            if(!req)
-            char[] inputArr = input.toCharArray();
-            //모음이 존재하고 && 세개가 연속되지 않고
+            if(!isIncludeVowel) isAcceptable = false;
+            print(isAcceptable, input);
+
         }
         System.out.println(sb);
 
     }
 
-    private static void requirement2(char[] inputArr) {
-        for (int i = 0; i < inputArr.length; i++) {
-            if(inputArr[i]=='a' &&)
-        }
-    }
-
-    private static boolean requirement3(String input) {
-        int originLength = input.length();
-        input = input.replace("aa","a");
-        input = input.replace("ii","i");
-        input = input.replace("uu","u");
-        System.out.println(input);
-        if(originLength != input.length()) return false;
-        return true;
+    private static void print(boolean isAcceptable, String input) {
+        sb.append("<").append(input).append("> is ");
+        if(!isAcceptable) sb.append("not ");
+        sb.append("acceptable.\n");
     }
 }
