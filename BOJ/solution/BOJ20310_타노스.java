619c50394f9ec814bb2f07283245a0488ad00b3d
Minji Ryu <77240765+mxnzx@users.noreply.github.com>
2024-02-21 11:36:41 +0900

    [BOJ]20310.타노스 - 문자열, 그리디

---
+++ BOJ/solution/BOJ20310_타노스.java
@@ -0,0 +1,53 @@
+package solution;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ20310_타노스 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String S = br.readLine();
+        int cnt0 = 0, cnt1 = 0;
+        for (int i = 0; i < S.length(); i++) {
+            int n = S.charAt(i);
+            if(n == '0') cnt0++;
+            else cnt1++;
+        }
+        cnt0 /= 2;
+        cnt1 /= 2;
+
+        // 1은 앞에서 부터 떼고
+        // 0은 뒤부터 뗀다
+        // 그리디
+
+        int tmp0 = 0, tmp1 = 0;
+        int[] idxArr = new int[cnt0 + cnt1];
+        int idx = 0;
+        for (int i = 0; i < S.length(); i++) {
+            if(tmp1 == cnt1) break;
+            if(S.charAt(i) == '1') {
+                idxArr[idx++] = i;
+                tmp1++;
+            }
+        }
+        for (int i = S.length()-1; i >= 0; i--) {
+            if(tmp0 == cnt0) break;
+            if(S.charAt(i) == '0') {
+                idxArr[idx++] = i;
+                tmp0++;
+            }
+        }
+
+        char[] arr = S.toCharArray();
+        for (int i = 0; i < idxArr.length; i++) {
+            arr[idxArr[i]] = '2';
+        }
+
+        StringBuilder sb= new StringBuilder();
+        for(char c : arr) {
+            if(c != '2') sb.append(c);
+        }
+        System.out.println(sb);
+    }
+}
