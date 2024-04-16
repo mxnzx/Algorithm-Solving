97376439f4534091857781e18eb4399390895139
Minji Ryu <77240765+mxnzx@users.noreply.github.com>
2024-04-16 17:38:50 +0900

    [BOJ]1676.팩토리얼0의개수 - 수학

---
+++ BOJ/solution/BOJ1676_팩토리얼0의개수.java
@@ -0,0 +1,18 @@
+package solution;
+
+import java.util.*;
+import java.io.*;
+
+public class BOJ1676_팩토리얼0의개수 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int cnt = 0;
+        while(n >= 5) {
+            cnt += n / 5;
+            n /= 5;
+            System.out.println(n);
+        }
+        System.out.println(cnt);
+    }
+}
