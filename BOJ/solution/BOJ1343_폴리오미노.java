f8f9444c15120c20752cf43e75363052b33a8c10
Minji Ryu <77240765+mxnzx@users.noreply.github.com>
2024-05-10 23:01:34 +0900

    [BOJ]1343.폴리오미노 - 구현, 그리디

--- BOJ/solution/BOJ1343_폴리오미노.java
+++ BOJ/solution/BOJ1343_폴리오미노.java
@@ -1,65 +1,48 @@
 package solution;
 
-import java.util.*;
 import java.io.*;
 
 public class BOJ1343_폴리오미노 {
+
+    static StringBuilder ans = new StringBuilder();
+
+    static boolean replacePolyomino(int cnt) {
+
+        if(cnt % 2 != 0) return false;
+
+        while(cnt >= 4) {
+            ans.append("AAAA");
+            cnt -= 4;
+        }
+        if(cnt == 2) ans.append("BB");
+        return true;
+    }
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String S = br.readLine();
         char[] input = S.toCharArray();
-        int s = 0;
-        int e = 3;
-        while(s < input.length) {
-            if(input[s] == '.') {
-                s++;
-                e++;
-                continue;
-            }
-            boolean flag = true;
-            if(e >= input.length) flag = false;
 
-            if(flag) {
-                for (int i = s; i <= e; i++) {
-                    if(input[i] == '.') {
-                        flag = false;
-                        break;
-                    }
-                }
-            }
-
-            if(!flag) {
-                e -= 2;
-                flag = true;
-                if(e >= input.length) flag = false;
-                if(flag) {
-                    for (int i = s; i <= e; i++) {
-                        if(input[i] == '.') {
-                            flag = false;
-                            break;
-                        }
-                    }
-                }
-
-                if(!flag) {
+        int cnt = 0;
+        for (int i = 0; i < input.length; i++) {
+            if(input[i] == 'X') cnt++;
+            else {
+                if(!replacePolyomino(cnt)) {
                     System.out.println(-1);
                     return;
                 } else {
-                    for (int i = s; i <= e; i++) {
-                        input[i] = 'B';
-                    }
-                }
-
-            } else {
-                for (int i = s; i <= e; i++) {
-                    input[i] = 'A';
+                    cnt = 0;
+                    ans.append(".");
                 }
             }
-            s = e + 1;
-            e += 4;
         }
-        System.out.println(String.valueOf(input));
+
+        if(!replacePolyomino(cnt)) {
+            System.out.println(-1);
+            return;
+        }
+
+        System.out.println(ans);
 
     }
 }
